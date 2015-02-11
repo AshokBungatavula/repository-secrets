71b8c0b9ad01d3c1ef1b93986bc4bdaebcdc5cf9
Sam Gleske <samuel_gleske@cable.comcast.com>
2015-02-11 16:55:14 -0500

    License updates for Comcast Cable
    Copyright 2014 Comcast Cable Communications Management, LLC
    Moved Sam Gleske's license into the NOTICES file.

--- interpolate_example.rb
+++ interpolate_example.rb
@@ -1,3 +1,24 @@
+#
+#Copyright 2015 Comcast Cable Communications Management, LLC
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+#Created by Sam Gleske
+#Wed Jan 14 22:04:55 EST 2015
+#Ubuntu 14.04.1 LTS
+#Linux 3.13.0-44-generic x86_64
+#ruby 1.9.3p484 (2013-11-22 revision 43786) [x86_64-linux]
+
 #http://stackoverflow.com/questions/9891708/ruby-file-encryption-decryption-with-private-public-keys
 #A ruby example of string interpolation with RSA encrypted secrets
 
